6477a18b4c525351a6d0c68a1fac270e45ad40c9
matfax <mat@fax.fyi>
2023-08-10 15:58:46 +0200

    fix(gcloud): migrate from exec.getExecOutput to exec.run

--- gcloud/action.js
+++ gcloud/action.js
@@ -5,25 +5,26 @@ const { parseArgsStringToArgv } = require('string-argv')
 const commandArgs = parseArgsStringToArgv(core.getInput('command'))
 const subCommandArgs = parseArgsStringToArgv(core.getInput('subcommand'))
 const options = {
-    ignoreReturnCode: true,
-    silent: true,
+    ignoreReturnCode: true
 }
 const args = [...commandArgs, ...subCommandArgs, '--quiet', '--format="json"']
 
-const output = await exec.getExecOutput('gcloud', args, options)
+exec.run('gcloud', args, options, (error, stdout) => {
+    if (error) {
+        if (!core.getBooleanInput('expect_failure')) {
+            core.setFailed(error);
+        }
+    } else {
+        if (core.getBooleanInput('expect_failure') && result.exitCode === 0) {
+            core.setFailed('expected failure but got success');
+        }
 
-const expectFailure = core.getBooleanInput('expect_failure');
-const expectOutput = core.getBooleanInput('expect_output');
-
-if (output.exitCode !== 0) {
-    if (!expectFailure) {
-        core.setFailed(output.stderr);
-    } else if (expectOutput && output.stdout === '') {
-        core.setFailed('expected output but got nothing');
+        if (core.getBooleanInput('expect_output')) {
+            const result = JSON.parse(stdout);
+            if (!result.output) {
+                core.setFailed('expected output but got nothing');
+            }
+            core.setOutput('result', result.output);
+        }
     }
-} else if (expectFailure) {
-    core.setFailed('expected failure but got success');
-}
-
-core.setOutput('exitCode', output.exitCode);
-core.setOutput('result', output.stdout);
+})
