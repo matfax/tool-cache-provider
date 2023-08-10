dba0e14feb1d3767ebc7a5bef94a875c1eb2ee40
matfax <mat@fax.fyi>
2023-08-10 15:34:45 +0200

    feat(gcloud): migrate from inline github-script to full action

---
+++ gcloud/action.js
@@ -0,0 +1,29 @@
+const core = require("@actions/core");
+const { exec } = require("@actions/exec");
+const { parseArgsStringToArgv } = require('string-argv')
+
+const commandArgs = parseArgsStringToArgv(core.getInput('command'))
+const subCommandArgs = parseArgsStringToArgv(core.getInput('subcommand'))
+const options = {
+    ignoreReturnCode: true,
+    silent: true,
+}
+const args = [...commandArgs, ...subCommandArgs, '--quiet', '--format="json"']
+
+const output = await exec.getExecOutput('gcloud', args, options)
+
+const expectFailure = core.getBooleanInput('expect_failure');
+const expectOutput = core.getBooleanInput('expect_output');
+
+if (output.exitCode !== 0) {
+    if (!expectFailure) {
+        core.setFailed(output.stderr);
+    } else if (expectOutput && output.stdout === '') {
+        core.setFailed('expected output but got nothing');
+    }
+} else if (expectFailure) {
+    core.setFailed('expected failure but got success');
+}
+
+core.setOutput('exitCode', output.exitCode);
+core.setOutput('result', output.stdout);
